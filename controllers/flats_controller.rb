db2a1a6c7abaa933b5bf83761441b04dcfc80c7c
Raphael Ugha <raphaelugha@gmail.com>
2018-07-23 17:58:25 +0100

    Controllers added index, create, show, new.

--- controllers/flats_controller.rb
+++ controllers/flats_controller.rb
@@ -1,13 +1,13 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require_relative( '../models/flat.rb' )
-
+#index
 get '/flats' do
 @flats = Flat.all()
 erb ( :"flats/index")
 end
 
-
+#show
 get '/flats/:id' do
 @flats = Flat.find(params['id'].to_i)
 erb(:"flats/show")
--- controllers/tenants_controller.rb
+++ controllers/tenants_controller.rb
@@ -2,12 +2,31 @@ require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require_relative( '../models/tenant.rb' )
 
+#index
 get '/tenants' do
   @tenants = Tenant.all
   erb(:"tenants/index")
 end
 
-get '/tenants' do
-  @tenants = Tenant.find(params['id'].to_i)
+#create
+post '/tenants' do
+  @tenant = Tenant.new(params)
+  @tenant.save()
+  erb :"tenants/create"
+end
+
+#new
+get '/tenants/new' do
+  erb(:"tenants/new")
+end
+
+#show
+get '/tenants/:id' do
+  @tenant = Tenant.find(params['id'].to_i)
   erb(:"tenants/show")
 end
+
+#
+# get '/tenants/login' do
+#   erb(:"tenants/login")
+# end
