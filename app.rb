fdfad123d06bd774f9f4e434ac12552c6445e235
Raphael Ugha <raphaelugha@gmail.com>
2018-07-24 12:55:16 +0100

    Added rentals_controller to app.rb

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require( 'sinatra/contrib/all' )
 require_relative( './controllers/flats_controller.rb' )
 require_relative( './controllers/flats_matches_controller.rb' )
 require_relative( './controllers/tenants_controller.rb' )
-
+require_relative( './controllers/rentals_controller.rb' )
 
 get '/' do
   erb( :index )
--- controllers/rentals_controller.rb
+++ controllers/rentals_controller.rb
@@ -2,6 +2,7 @@ require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require_relative( '../models/flat.rb' )
 require_relative( '../models/tenant.rb' )
+also_reload('./models/*')
 
 #index
 get '/rentals' do
